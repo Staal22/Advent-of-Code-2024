efd4b14ad382e5297e8227639816bb8ae834d997
Staal22 <staalbr@gmail.com>
2024-12-09 20:18:58 +0100

    Start working on day 3 part 2

--- 3dec/instruction_parser.cpp
+++ 3dec/instruction_parser.cpp
@@ -6,29 +6,71 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <set>
 
 int instruction_parser::sum_multiply_input()
 {
     // Format
-    // foobar{}124fk,<>'[mul(123,3)f42mul(5*,55)
+    // 2don't()foobar{}1mul(1,1)24fk,<>'[mu()l(123,3)f42mul(5*,535)3mul(77,5)
 
     int sum = 0;
 
-    std::regex pattern(
-        R"(mul\((\d+),(\d+)\))");
+    std::regex mul_pattern(R"(mul\((\d+),(\d+)\))");
+    std::regex do_pattern(R"((do\(\)))");
+    std::regex dont_pattern(R"((don't\(\)))");
 
     std::ifstream file("input.txt");
     std::string line;
     while (std::getline(file, line))
     {
-        // Extract "mul(int,int)"
-        auto line_begin = std::sregex_iterator(line.begin(), line.end(), pattern);
-        auto line_end = std::sregex_iterator();
+        // Track all instructions (with data if applicable)
+        std::vector<std::tuple<int, std::string, int, int>> instructions;
 
-        for (std::sregex_iterator i = line_begin; i != line_end; ++i)
+        auto collect_instructions = [&](const std::regex& pattern, const std::string& type)
         {
-            const std::smatch& match = *i;
-            sum += std::stoi(match[1]) * std::stoi(match[2]);
+            auto line_begin = std::sregex_iterator(line.begin(), line.end(), pattern);
+            auto line_end = std::sregex_iterator();
+
+            for (std::sregex_iterator i = line_begin; i != line_end; ++i)
+            {
+                if (type == "mul")
+                {
+                    int arg1 = std::stoi((*i)[1]);
+                    int arg2 = std::stoi((*i)[2]);
+                    instructions.emplace_back(i->position(), type, arg1, arg2);
+                }
+                else
+                {
+                    instructions.emplace_back(i->position(), type, 0, 0);
+                }
+            }
+        };
+
+        collect_instructions(mul_pattern, "mul");
+        collect_instructions(do_pattern, "do");
+        collect_instructions(dont_pattern, "dont");
+
+        // Sort instructions by order of appearance
+        std::ranges::sort(instructions);
+
+        // Track enabled state and process enabled multiplies
+        bool enabled = true;
+        std::set<int> processed_positions;
+
+        for (const auto& [position, type, arg1, arg2]: instructions)
+        {
+            if (type == "do")
+                enabled = true;
+            else if (type == "dont")
+                enabled = false;
+            else if (type == "mul" && enabled)
+            {
+                if (processed_positions.insert(position).second) // Only process if not already done
+                {
+                    sum += arg1 * arg2;
+                }
+                // sum += arg1 * arg2;
+            }
         }
     }
 
